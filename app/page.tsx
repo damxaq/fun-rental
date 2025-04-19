5f5c5840807734286e31dd8c1e3e0fb54db0f469
Damian Celico <damiancelico@gmail.com>
2025-04-19 21:09:49 +0200

    favorites page view

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,10 @@ async function ShowItems({
   return (
     <>
       {data.length === 0 ? (
-        <NoItems />
+        <NoItems
+          title="Sorry, no listings found for this category..."
+          description="Please check other category or create your own listing!"
+        />
       ) : (
         <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
           {data.map((item: any) => (

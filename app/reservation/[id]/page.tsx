c89b75c13a8bc7264681e4bf25ee2cf424311925
Damian Celico <damiancelico@gmail.com>
2025-05-28 20:35:10 +0200

    reservation screen skeleton

--- app/reservation/[id]/page.tsx
+++ app/reservation/[id]/page.tsx
@@ -1,13 +1,17 @@
 import { CategoryShowcase } from "@/app/components/CategoryShowcase";
-import { OfferMap } from "@/app/components/OfferMap";
 import { useCountries } from "@/app/lib/getCountries";
 import { Separator } from "@/components/ui/separator";
 import prisma from "@/lib/prisma";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import Image from "next/image";
 import { unstable_noStore as noStore } from "next/cache";
-import ImageGallery from "@/app/components/ImageGallery";
 import { redirect } from "next/navigation";
+
+const StatusType: { [key: string]: string } = {
+  Pending: "text-blue-500",
+  Confirmed: "text-green-500",
+  Declined: "text-red-500",
+};
 
 async function getData(reservationId: string) {
   noStore();
@@ -19,6 +23,8 @@ async function getData(reservationId: string) {
       startDate: true,
       endDate: true,
       status: true,
+      // how many guests
+      // who is renting
       User: {
         select: {
           id: true,
@@ -35,6 +41,7 @@ async function getData(reservationId: string) {
           description: true,
           price: true,
           title: true,
+          categoryName: true,
         },
       },
     },
@@ -54,53 +61,61 @@ export default async function ReservationDetailsRoute({
   if (!user) return redirect("/");
   const { id } = await params;
   const data = await getData(id);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { getCountryByValue } = useCountries();
+  const country = getCountryByValue(data.Vehicle.country);
 
   return (
-    <div className="w-[90%] lg:w-[75%] mx-auto mt-10 mb-20">
-      <p>
-        {data.User.firstName} {data.User.lastName}
-      </p>
-      <img src={data.User.profileImage} alt="profileImage" />
-      <p>
-        {data.startDate.toLocaleDateString()} -{" "}
-        {data.endDate.toLocaleDateString()}
-      </p>
-      <h1 className="font-medium text-2xl mb-5">{data.Vehicle.title}</h1>
-      {/* <ImageGallery gallery={[data.photo, ...data.gallery]} />
-      <div className="flex justify-between gap-x-24 mt-8 flex-col lg:flex-row">
-        <div className="lg:w-2/3 w-full">
-          <h3 className="text-xl font-medium">
-            {country?.flag} {country?.label} / {country?.region}
-          </h3>
-          <div className="flex gap-x-2 text-muted-foreground">
-            <p>{data.guests} Guests</p>
-          </div>
-          <div className="flex items-center mt-6">
-            <Image
-              src={
-                data.User.profileImage ??
-                "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
-              }
-              alt="User Profile"
-              className="w-11 h-11 rounded-full"
-              width={100}
-              height={100}
-            />
-            <div className="flex flex-col ml-4">
-              <h3 className="font-medium">Hosted by {data.User.firstName}</h3>
-              <p className="text-sm text-muted-foreground">Host since 2020</p>
+    <div className="w-[90%] lg:w-[75%] mx-auto mt-10 mb-20 flex">
+      <div className="w-1/2">
+        <div className="relative h-52">
+          <Image
+            src={`https://ulsjeycbmhzuambfgfob.supabase.co/storage/v1/object/public/images/${data.Vehicle.photo}`}
+            alt="Offer Image"
+            fill
+            className="rounded-lg h-full object-cover"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            priority={true}
+          />
+        </div>
+        <div className="flex justify-between gap-x-24 mt-8 flex-col lg:flex-row">
+          <div className="w-full">
+            <h3 className="text-xl font-medium">{data.Vehicle.title}</h3>
+            <Separator className="my-2" />
+            <h3 className="text-lg font-medium">
+              {country?.label} / {country?.region}
+            </h3>
+            <div className="flex items-center mt-6">
+              <Image
+                src={
+                  data.User.profileImage ??
+                  "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
+                }
+                alt="User Profile"
+                className="w-11 h-11 rounded-full"
+                width={100}
+                height={100}
+              />
+              <div className="flex flex-col ml-4">
+                <h3 className="font-medium">Hosted by {data.User.firstName}</h3>
+                <p className="text-sm text-muted-foreground">Host since 2020</p>
+              </div>
+            </div>
+            <Separator className="my-2" />
+
+            <CategoryShowcase categoryName={data.Vehicle.categoryName} />
+            <Separator className="my-2" />
+            <span className={`font-medium ${StatusType[data.status]}`}>
+              {data.status}
+            </span>
+            <Separator className="my-2" />
+            <div className="flex gap-x-2 text-muted-foreground">
+              <p>{data.guests} Guests</p>
             </div>
           </div>
-
-          <Separator className="my-7" />
-
-          <CategoryShowcase categoryName={data.categoryName} />
-          <Separator className="my-7" />
-          <p className="text-muted-foreground">{data.description}</p>
-          <Separator className="my-7" />
-          <OfferMap locationValue={country?.value as string} />
         </div>
-      </div> */}
+      </div>
+      <div className="w-full h-full mx-6 px-4 border-2 rounded-md">Chat</div>
     </div>
   );
 }
